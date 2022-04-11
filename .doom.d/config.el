a31bec3abed6a5308f75c3efdbc91752e28351e0
Ray Thomas <Ray.Thomas@dynetics.com>
2022-04-11 16:03:26 -0500

    Change email to work email for magit

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Ray Thomas"
-      user-mail-address "mhsbari@gmail.com")
+      user-mail-address "Ray.Thomas@Dynetics.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
